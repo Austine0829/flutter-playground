c9f2cb50780177e9fe709f5b82ef3b55bfb62996
Austine0829 <audreytuazon29@gmail.com>
2026-01-20 08:24:25 +0800

    (add) var samples

---
+++ dart_basics/variable.dart
@@ -0,0 +1,18 @@
+
+import 'dart:ffi';
+
+void main() {
+  // int x = 10;
+  // int y = 20;
+  // String result = "X + Y is equals to ${x + y}";
+
+  // print(result);
+
+  double x = 99.99;
+
+  print(x);
+
+  bool isMinor = true;
+
+  print(isMinor);
+}
