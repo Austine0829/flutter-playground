f07a5faf8f9f779d696060baf76d6eec64b86830
Austine0829 <audreytuazon29@gmail.com>
2026-01-20 09:18:04 +0800

    (add) collection samples

---
+++ dart_basics/collections.dart
@@ -0,0 +1,28 @@
+void main() {
+  print("List");
+  List<String> names = ["Austine", "Audrey", "Tuazon"];
+
+  print(names);
+
+  print("\nMap");
+  Map<int, String> employees = {
+    1143: "Austine Tuazon",
+    3324: "Audrey Basilio"
+  };
+
+  print(employees);
+  print(employees[1143]);
+
+  print("\nSet");
+  Set<int> numbers = {1, 2, 3, 4, 5, 5};
+
+  print(numbers);
+
+  print("\nCollection with if statement and spread operator");
+  bool showExtra = true;
+  List<int> otherItems = [4, 5, 6];
+
+  List<int> items = [1, 2, 3, if (showExtra) ...otherItems];
+
+  print(items);
+}
