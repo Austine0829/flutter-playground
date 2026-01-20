59d4e48b5eb919026cba239d596b635cbbd26ed8
Austine0829 <audreytuazon29@gmail.com>
2026-01-20 09:29:53 +0800

    (add) null checks samples

---
+++ dart_basics/null_safety.dart
@@ -0,0 +1,14 @@
+void main() {
+  // Null Assertion
+  // String? name;
+
+  // print(name!);
+
+  // Nullable
+  String? lastName;
+
+  print(lastName);
+
+  // Coalescing
+  print(lastName ?? "Last Name");
+}
