4ef244a80626c6bb8985ded1616acf5f6388b089
Austine0829 <audreytuazon29@gmail.com>
2026-01-27 17:55:02 +0800

    (add) persistent bottom nav

--- flutter_basics/lib/main.dart
+++ flutter_basics/lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_basics/utils/app_text_theme.dart';
+import 'package:flutter_basics/widgets/horizontal_scroll_tile_holder.dart';
+import 'package:flutter_basics/widgets/vertical_scroll_tile_holder.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -8,8 +12,71 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: HomePage(),
+    return MaterialApp(
+      theme: ThemeData(
+        useMaterial3: true,
+      ),
+      home: const PersistentBottomNavigationBar(),
+    );
+  }
+}
+
+class PersistentBottomNavigationBar extends StatefulWidget {
+  const PersistentBottomNavigationBar({super.key});
+
+  @override
+  State<PersistentBottomNavigationBar> createState() => _PersistentBottomNavigationBarState();
+}
+
+class _PersistentBottomNavigationBarState extends State<PersistentBottomNavigationBar> {
+  int _currentIndex = 0;
+
+  static const List<Widget> _pages = [
+    HomePage(),
+    Center(child: Text("Search"))
+  ] ;
+
+  @override 
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _pages[_currentIndex],
+      extendBody: true,
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.7),
+              blurRadius: 30,
+              spreadRadius: 15
+            )
+          ]
+        ),
+        child: BottomNavigationBar(
+          backgroundColor: Colors.transparent,
+          elevation: 15,
+          currentIndex: _currentIndex,
+          selectedFontSize: 10,
+          unselectedFontSize: 10,
+          selectedItemColor: Colors.white,
+          unselectedItemColor: Colors.grey,
+          onTap: (index) {
+            setState(() {
+              _currentIndex = index;
+            });
+          },
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              activeIcon: Icon(Icons.home),
+              label: "Home"
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.search),
+              label: "Search"
+            )
+          ]
+        ) 
+      ) 
     );
   }
 }
@@ -22,116 +89,42 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.black,
       appBar: AppBar(
-        title: const Text("Home", style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white)),
         backgroundColor: Colors.black,
+        title: const Text(
+          "Home", 
+          style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: Colors.white
+            )
+          )
         ),
       body: Padding(
         padding: const EdgeInsets.only(
           left: 17,
-          right: 17
+          right: 17,
+          top: 5
         ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const Text("Naruto Shippuden", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.white)),
-            SizedBox(
-            height: 200,
-            child: ListView.separated(
-            scrollDirection: Axis.horizontal,
-            itemBuilder:(context, index) {
-                return Container(
-                width: 150,
-                decoration: BoxDecoration(
-                  border: Border.all(width: 1),
-                  borderRadius: BorderRadius.circular(8),
-                  color: Colors.red
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Container(
-                      height: 150,
-                      decoration: BoxDecoration(
-                        image: const DecorationImage(
-                          image: AssetImage("assets/naruto.jpeg"),
-                          fit: BoxFit.cover
-                        ),
-                      borderRadius: BorderRadius.circular(8) 
-                      ),
-                    ),
-                  const Text("Anime", style: TextStyle(color: Colors.white54)),
-                  const Text("Naruto", style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white))
-                  ]
+        child: SingleChildScrollView(child:
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text("Anime Episodes", style: Theme.of(context).textTheme.sectionLabel),
+              const HorizontalScrollTileHolderWidget(),
+              const SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                Text("Recent Watch History", style: Theme.of(context).textTheme.sectionLabel),
+                TextButton(
+                  onPressed: () {}, 
+                  child: const Text("Show All")
                 )
-              );
-            }, 
-            separatorBuilder: (_, __) => const SizedBox(width: 10), 
-            itemCount: 5), 
-            ) 
-          ],
+              ]),
+              const VerticalScrollTileHolderWidget(),
+              const HorizontalScrollTileHolderWidget(),
+          ]),
         ) 
-      ) 
+      ),
     );
   }
 }
-
-// class HomePage extends StatelessWidget {
-//   const HomePage({super.key});
-
-//   @override 
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       backgroundColor: Colors.black,
-//       appBar: AppBar(
-//         title: const Text("Home", style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white)),
-//         backgroundColor: Colors.black,
-//         ),
-//       body: Padding(
-//         padding: const EdgeInsets.only(
-//           left: 17,
-//           right: 17
-//         ),
-//         child: Column(
-//           crossAxisAlignment: CrossAxisAlignment.start,
-//           children: [
-//             const Text("Naruto Shippuden", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.white)),
-//             SizedBox(
-//             height: 200,
-//             child: ListView.separated(
-//             scrollDirection: Axis.horizontal,
-//             itemBuilder:(context, index) {
-//                 return Container(
-//                 width: 150,
-//                 decoration: BoxDecoration(
-//                   border: Border.all(width: 1),
-//                   borderRadius: BorderRadius.circular(8),
-//                   color: Colors.red
-//                 ),
-//                 child: Column(
-//                   crossAxisAlignment: CrossAxisAlignment.start,
-//                   children: [
-//                     Container(
-//                       height: 150,
-//                       decoration: BoxDecoration(
-//                         image: const DecorationImage(
-//                           image: AssetImage("assets/naruto.jpeg"),
-//                           fit: BoxFit.cover
-//                         ),
-//                       borderRadius: BorderRadius.circular(8) 
-//                       ),
-//                     ),
-//                   const Text("Anime", style: TextStyle(color: Colors.white54)),
-//                   const Text("Naruto", style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white))
-//                   ]
-//                 )
-//               );
-//             }, 
-//             separatorBuilder: (_, __) => const SizedBox(width: 10), 
-//             itemCount: 5), 
-//             ) 
-//           ],
-//         ) 
-//       ) 
-//     );
-//   }
-// }
